96a81b746e8eb19095659c9fecd895ca94ec4c1e
Suraj Bera <Surajjberaa@users.noreply.github.com>
2024-03-01 20:59:59 +0530

    Created custom react and understood the working  of react props and components

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import Chai from './chai'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const username = "Chai aur code"
 
   return (
     <>
     <Chai/>
-    <h1>Chai aur react</h1>
+    <h1>Chai aur react by {username}</h1>
     <p>test paragraph</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props:{
+//       href: 'https://google.com',
+//       target: '_blank'    
+//   },
+//   children: 'Click me to visit Google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "Chai aur React"
+
+const ReactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+    target: '_blank' 
+  },
+  'Click to visit google',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    ReactElement
+
 )
